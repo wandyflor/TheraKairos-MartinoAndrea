5d16d1f2e53625517134f954dba4f23c21b308e2
Andrea Martino <111247451+wandyflor@users.noreply.github.com>
2025-06-07 20:21:25 -0300

    Delete therapistapp/interfaces directory

--- therapistapp/interfaces/ICalendarSelectedListener.java
+++
@@ -1,9 +0,0 @@
-package com.application.interfaces;
-
-import com.application.view.panels.consultation.calendar.ModelDate;
-import java.awt.event.MouseEvent;
-
-public interface ICalendarSelectedListener {
-
-    public void selected(MouseEvent evt, ModelDate date);
-}
--- therapistapp/interfaces/IConsultationPatientActionsEvent.java
+++
@@ -1,9 +0,0 @@
-package com.application.interfaces;
-
-public interface IConsultationPatientActionsEvent {
-
-    public void onDelete(String patientId);
-
-    public void onView(String patientId);
-    
-}
